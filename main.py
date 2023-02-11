48fb89e7e7e9d104c1eef4d2fcb57039c29d1864
Dāniels Čulka <daniels.culka@edu.rtu.lv>
2023-02-11 17:35:39 +0200

    Changes

--- main.py
+++ main.py
@@ -14,17 +14,34 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
+            if not len(opening_brackets_stack):
+                return i
+
+            last_opening_bracket = opening_brackets_stack[len(opening_brackets_stack) - 1]
+
+            if are_matching(last_opening_bracket[0], next):
+                opening_brackets_stack.remove(last_opening_bracket)
+            else:
+                return i
+                
             pass
+
+    return -1
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    if mismatch == -1:
+        print("Success")
+    else:
+        print(mismatch + 1)
 
 
 if __name__ == "__main__":
